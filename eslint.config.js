addf321ea40b478ca808316b965fbfddb7f8eb7f
BeFinkler <finklerbernardo@gmail.com>
2026-09-03 12:55:09 -0300

    estrutura: configura cliente React com Vite e ambiente independente

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import hooks from 'eslint-plugin-react-hooks';
+import refresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { ignores: ['node_modules/**', '.local/**', 'dist/**', '.vercel/**', 'playwright-report/**', 'test-results/**'] },
+  js.configs.recommended,
+  { files: ['**/*.{js,jsx}'], languageOptions: { ecmaVersion: 'latest', sourceType: 'module', globals: { ...globals.browser, ...globals.node }, parserOptions: { ecmaFeatures: { jsx: true } } }, rules: { 'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z]', argsIgnorePattern: '^_' }] } },
+  { files: ['src/**/*.{js,jsx}'], plugins: { 'react-hooks': hooks, 'react-refresh': refresh }, rules: { ...hooks.configs.recommended.rules, 'react-refresh/only-export-components': ['warn', { allowConstantExport: true }] } },
+];
